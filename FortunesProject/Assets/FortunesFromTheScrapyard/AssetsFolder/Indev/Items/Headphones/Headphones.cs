0f424407917900d07485c286272aa0427174930c
EarthZebra <aus10.hawkeye2@gmail.com>
2024-07-24 16:51:41 -0500

    Progress on Headphones

--- FortunesProject/Assets/FortunesFromTheScrapyard/AssetsFolder/Indev/Items/Headphones/Headphones.cs
+++ FortunesProject/Assets/FortunesFromTheScrapyard/AssetsFolder/Indev/Items/Headphones/Headphones.cs
@@ -3,19 +3,25 @@ using RoR2.ContentManagement;
 using MSU.Config;
 using RoR2.Items;
 using MSU;
+using R2API;
+using UnityEngine;
+using JetBrains.Annotations;
+using UnityEngine.Networking;
 
 namespace FortunesFromTheScrapyard.Items
 {
     public class Headphones : ScrapyardItem
     {
         [ConfigureField(ScrapyardConfig.ID_ITEMS)]
-        public static float chanceBase = 17f;
-        [ConfigureField(ScrapyardConfig.ID_ITEMS)]
-        public static float chanceStack = 10f;
-        [ConfigureField(ScrapyardConfig.ID_ITEMS)]
-        public static float disorientDuration = 2f;
+        public static float chanceBase = 10f;
         [ConfigureField(ScrapyardConfig.ID_ITEMS)]
         public static float disorientDamage = 0.2f;
+
+        public static float sprayDamage = 2.5f;
+
+        public static float sprayRaduisBase = 10f;
+        public static float sprayRaduisStack = 2f;
+
         public override void Initialize()
         {
         }
@@ -41,28 +47,38 @@ namespace FortunesFromTheScrapyard.Items
             public static ItemDef GetItemDef() => ScrapyardContent.Items.Headphones;
             public void OnDamageDealtServer(DamageReport damageReport)
             {
-                var victimBody = damageReport.victimBody;
-                if (!victimBody.HasBuff(ScrapyardContent.Buffs.bdDisorient))
+                if (!NetworkServer.active) { return; }
+                CharacterBody attackerBody = damageReport.attacker.GetComponent<CharacterBody>();
+                CharacterBody victimBody = damageReport.victimBody;
+                if (attackerBody)
                 {
-                    float procChance = GetStackValue(chanceBase, chanceStack, stack) * damageReport.damageInfo.procCoefficient;
-                    float adjustedProcChance = Util.ConvertAmplificationPercentageIntoReductionPercentage(procChance);
-                    if (Util.CheckRoll(adjustedProcChance, damageReport.attackerMaster))
+                    if (victimBody.HasBuff(ScrapyardContent.Buffs.bdDisorient))
                     {
-                        victimBody.AddTimedBuff(ScrapyardContent.Buffs.bdDisorient, disorientDuration);
-                    }
-                }
-            }
-        }
+                        victimBody.RemoveBuff(ScrapyardContent.Buffs.bdDisorient);
 
-        public class HeadphonesBuffBehaviour : BaseBuffBehaviour, IOnIncomingDamageServerReceiver
-        {
-            [BuffDefAssociation]
-            public static BuffDef GetBuffDef() => ScrapyardContent.Buffs.bdDisorient;
-            public void OnIncomingDamageServer(DamageInfo damageInfo)
-            {
-                if(CharacterBody.HasBuff(ScrapyardContent.Buffs.bdDisorient))
-                {
-                    damageInfo.damage *= 1 + disorientDamage;
+                        BlastAttack blastAttack = new BlastAttack();
+
+                        blastAttack.attacker = attackerBody.gameObject;
+                        blastAttack.inflictor = null;
+                        blastAttack.teamIndex = attackerBody.teamComponent.teamIndex;
+                        blastAttack.baseDamage = damageReport.damageDealt * Headphones.GetStackValue(sprayDamage, sprayDamage, attackerBody.inventory.GetItemCount(ScrapyardContent.Items.Headphones));
+                        blastAttack.baseForce = 100f;
+                        blastAttack.position = base.transform.position;
+                        blastAttack.radius = 15f + Headphones.GetStackValue(sprayRaduisBase, sprayRaduisStack, attackerBody.inventory.GetItemCount(ScrapyardContent.Items.Headphones));
+                        blastAttack.falloffModel = BlastAttack.FalloffModel.None;
+                        // blastAttack.bonusForce = Vector3.zero;
+                        blastAttack.damageType = DamageType.Generic;
+                        blastAttack.Fire();
+                    }
+                    else if (!victimBody.HasBuff(ScrapyardContent.Buffs.bdDisorient))
+                    {
+                        float procChance = chanceBase * damageReport.damageInfo.procCoefficient;
+                        float adjustedProcChance = Util.ConvertAmplificationPercentageIntoReductionPercentage(procChance);
+                        if (Util.CheckRoll(adjustedProcChance, damageReport.attackerMaster))
+                        {
+                            victimBody.AddBuff(ScrapyardContent.Buffs.bdDisorient);
+                        }
+                    }
                 }
             }
         }
