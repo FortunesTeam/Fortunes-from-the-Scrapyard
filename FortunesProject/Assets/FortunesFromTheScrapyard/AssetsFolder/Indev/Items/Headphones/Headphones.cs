7982b6e6b6bc237d7ac8dc08f160df2facf02002
EarthZebra <aus10.hawkeye2@gmail.com>
2024-07-24 21:11:32 -0500

    Headphones almost sorta complete

--- FortunesProject/Assets/FortunesFromTheScrapyard/AssetsFolder/Indev/Items/Headphones/Headphones.cs
+++ FortunesProject/Assets/FortunesFromTheScrapyard/AssetsFolder/Indev/Items/Headphones/Headphones.cs
@@ -12,18 +12,32 @@ namespace FortunesFromTheScrapyard.Items
 {
     public class Headphones : ScrapyardItem
     {
+        public const string TOKEN = "SCRAPYARD_ITEM_HEADPHONES_DESC";
+
         [ConfigureField(ScrapyardConfig.ID_ITEMS)]
+        [FormatToken(TOKEN, 0)]
         public static float chanceBase = 10f;
+
         [ConfigureField(ScrapyardConfig.ID_ITEMS)]
-        public static float disorientDamage = 0.2f;
+        [FormatToken(TOKEN, FormatTokenAttribute.OperationTypeEnum.MultiplyByN, 100, 1)]
+        public static float headphoneBaseDamage = 1.2f;
 
-        public static float sprayDamage = 2.5f;
+        [ConfigureField(ScrapyardConfig.ID_ITEMS)]
+        [FormatToken(TOKEN, FormatTokenAttribute.OperationTypeEnum.MultiplyByN, 100, 2)]
+        public static float headphoneDamageStack = 1.2f;
 
-        public static float sprayRaduisBase = 10f;
-        public static float sprayRaduisStack = 2f;
+        [ConfigureField(ScrapyardConfig.ID_ITEMS)]
+        [FormatToken(TOKEN, 3)]
+        public static float headphoneRaduisBase = 10f;
+        [ConfigureField(ScrapyardConfig.ID_ITEMS)]
+        [FormatToken(TOKEN, 4)]
+        public static float headphoneRaduisStack = 2f;
+
+        public static GameObject headphonesShockwavePrefab;
 
         public override void Initialize()
         {
+            headphonesShockwavePrefab = AssetCollection.FindAsset<GameObject>("HeadphoneShockwaveEffect");
         }
 
         public override bool IsAvailable(ContentPack contentPack)
@@ -58,17 +72,26 @@ namespace FortunesFromTheScrapyard.Items
 
                         BlastAttack blastAttack = new BlastAttack();
 
+                        blastAttack.procCoefficient = 0.2f;
                         blastAttack.attacker = attackerBody.gameObject;
                         blastAttack.inflictor = null;
                         blastAttack.teamIndex = attackerBody.teamComponent.teamIndex;
-                        blastAttack.baseDamage = damageReport.damageDealt * Headphones.GetStackValue(sprayDamage, sprayDamage, attackerBody.inventory.GetItemCount(ScrapyardContent.Items.Headphones));
+                        blastAttack.baseDamage = damageReport.damageDealt * Headphones.GetStackValue(headphoneBaseDamage, headphoneDamageStack, attackerBody.inventory.GetItemCount(ScrapyardContent.Items.Headphones));
                         blastAttack.baseForce = 100f;
                         blastAttack.position = base.transform.position;
-                        blastAttack.radius = 15f + Headphones.GetStackValue(sprayRaduisBase, sprayRaduisStack, attackerBody.inventory.GetItemCount(ScrapyardContent.Items.Headphones));
+                        blastAttack.radius = 15f + Headphones.GetStackValue(headphoneRaduisBase, headphoneRaduisStack, attackerBody.inventory.GetItemCount(ScrapyardContent.Items.Headphones));
                         blastAttack.falloffModel = BlastAttack.FalloffModel.None;
                         // blastAttack.bonusForce = Vector3.zero;
-                        blastAttack.damageType = DamageType.Generic;
+                        blastAttack.damageType = DamageType.Shock5s;
                         blastAttack.Fire();
+                        
+                        EffectManager.SpawnEffect(headphonesShockwavePrefab, new EffectData
+                        {
+                            origin = victimBody.transform.position,
+                            rotation = Quaternion.identity,
+                            scale = 1
+                        }, false);
+
                     }
                     else if (!victimBody.HasBuff(ScrapyardContent.Buffs.bdDisorient))
                     {
